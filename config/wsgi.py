a498bf45e19ff6c57d4d1cae73d3f7add0854481
Matthew Kriel <mckriel@gmail.com>
2024-06-29 21:33:56 +0200

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
